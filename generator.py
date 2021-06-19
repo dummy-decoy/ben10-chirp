588aacaea1c941cfffd795738f7dfbb2edd58fb0
rien <software@stochastique.net>
2021-06-19 12:36:10 +0200

    first commit

---
+++ generator.py
@@ -0,0 +1,38 @@
+import sys
+import math
+import struct
+import wave
+
+samplerate = 48000
+framerate = 24
+carrier = 15000
+symbols = (14000,14500,15500,16000)
+
+def tone(frequency):
+    samples = list(math.sin((tone.phase+n*1/samplerate*frequency)*2*math.pi) for n in range(int(1/framerate*samplerate)))
+    tone.phase += len(samples)*1/samplerate*frequency
+    return samples
+tone.phase = 0.0
+
+def generate(code):
+    result = list()
+    result.extend(tone(carrier))
+    for char in code:
+        result.extend(tone(symbols[int(char,4)]))
+        result.extend(tone(carrier))
+    return result
+
+def main():
+    code = sys.argv[1]
+    print('generating ben10-chirp for code:', code)
+
+    samples = generate(code)
+    data = bytes.join(b'', (struct.pack('<h',int(sample*32767)) for sample in samples))
+
+    output = wave.open(code+'.wav', 'wb')
+    output.setparams((1,2,samplerate,0,'NONE',''))
+    output.writeframes(data)
+    output.close()
+
+if __name__ == '__main__':
+    main()
