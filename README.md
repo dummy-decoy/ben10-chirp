80ca2d3784ed266e28f8ed58475bc503361d11c5
rien <software@stochastique.net>
2021-06-21 00:34:34 +0200

    add wildcards to codes on command line arguments

--- README.md
+++ README.md
@@ -7,19 +7,19 @@ Ben10 Omnitrix is an interactive toy which reacts to some sounds embedded inside
 
 ### generator
 
+```bash
+> python3 generator.py --help
+Usage: generator.py [options] (code)+
+
 generate waveforms corresponding to the given codes which trigger actions on a ben10 omnitrix toy device.
 
-code is a string of base 4 digit (0,1,2,3) of any length forming a code to generate. you can specify multiple codes, they will be either played in sequence or saved individually as wave files.
+code is a string of base 4 digit (0,1,2,3) of any length forming a code to generate. you can specify multiple codes, they will be either played in sequence or saved individually as wave files. you can also use a asterisk (*) as a wildcard for any digit of the code, the generator will then generate multiple codes going through all posible values for this digit.
 
-```Usage: generator.py [options] (code)+
 Options:
-  -h, --help            
-                        show this help message and exit
-  -e, --export          
-                        export the generated waveforms as .wav file named
+  -h, --help            show this help message and exit
+  -e, --export          export the generated waveforms as .wav file named
                         after each code into the current folder.
-  -p, --play            
-                        play the generated waveforms on the default system
+  -p, --play            play the generated waveforms on the default system
                         soundcard. each code is played one after the other,
                         with a pause inbetween each. the pause time is
                         controlled with the -t option. play is the default
@@ -28,8 +28,9 @@ Options:
                         duration in seconds of the pause interval between each
                         waveform when playing. (default: 1 s)
   -s SAMPLERATE, --samplerate=SAMPLERATE
-                        sample rate used for waveform generation.
-                        (default: 48000 hz)
+                        sample rate used for waveform generation.(default:
+                        48000 hz)
+
 ```
 
 ### extractor
@@ -37,7 +38,7 @@ Options:
 the `extractor.py` script will extracts the code from a given wave file.
 
 ```bash
-> python3 extractor.py "\058 Wildmutt SE 15kHz.wav.wav"
+> python3 extractor.py "058 Wildmutt SE 15kHz.wav.wav"
 ```
 
 ### generate-random
--- generator.py
+++ generator.py
@@ -46,26 +46,43 @@ def save(data, filename):
     file.write(data)
     file.close()
 
+def getcodes(code, prefix=''):
+    generated = prefix[:]
+    if len(code)==0:
+        yield prefix
+    elif code[0] in ('0','1','2','3'):
+        generated += code[0]
+        yield from getcodes(code[1:], generated)
+    elif code[0] == '*':
+        for wildcard in ('0','1','2','3'):
+            generated += wildcard
+            yield from getcodes(code[1:], generated)
+            generated = generated[:-1]
+    else:
+        raise ValueError('invalid character in code: '+code[0]+'. allowed characters are \'0\',\'1\',\'2\',\'3\' and \'.\'')
+
 def main():
-    parser = optparse.OptionParser('usage: %prog [options] (code)+ \n\ngenerate waveforms corresponding to the given codes which trigger actions on a ben10 omnitrix toy device.\n\ncode is a string of base 4 digit (0,1,2,3) of any length forming a code to generate. you can specify multiple codes, they will be either played in sequence or saved individually as wave files.')
+    parser = optparse.OptionParser('usage: %prog [options] (code)+ \n\ngenerate waveforms corresponding to the given codes which trigger actions on a ben10 omnitrix toy device.\n\ncode is a string of base 4 digit (0,1,2,3) of any length forming a code to generate. you can specify multiple codes, they will be either played in sequence or saved individually as wave files. you can also use a asterisk (*) as a wildcard for any digit of the code, the generator will then generate multiple codes going through all posible values for this digit.')
     parser.add_option('-e', '--export',     dest='export',     action='store_true', default=False, help='export the generated waveforms as .wav file named after each code into the current folder.')
     parser.add_option('-p', '--play',       dest='play',       action='store_true', default=False, help='play the generated waveforms on the default system soundcard. each code is played one after the other, with a pause inbetween each. the pause time is controlled with the -t option. play is the default action if none of play or export are specified.')
     parser.add_option('-t', '--pause',      dest='pause',      type='int',          default=1,     help='duration in seconds of the pause interval between each waveform when playing. (default: %default s)')   
     parser.add_option('-s', '--samplerate', dest='samplerate', type='int',          default=48000, help='sample rate used for waveform generation.(default: %default hz)') 
     (options, args) = parser.parse_args()
 
+    global samplerate
     samplerate = options.samplerate
 
-    for code in args:
-        print('generating ben10-chirp code:', code)
-        samples = generate(code)
-        data = wavefile(samples, samplerate)
-        if options.export:
-            save(data.getbuffer(), code+'.wav')
-        if options.play or (not options.play and not options.export):
-            play(data.getbuffer())
-            time.sleep(options.pause)
-        data.close()
+    for arg in args:
+        for code in getcodes(arg):
+            print('generating ben10-chirp code:', code)
+            samples = generate(code)
+            data = wavefile(samples, samplerate)
+            if options.export:
+                save(data.getbuffer(), code+'.wav')
+            if options.play or (not options.play and not options.export):
+                play(data.getbuffer())
+                time.sleep(options.pause)
+            data.close()
 
 if __name__ == '__main__':
     main()
